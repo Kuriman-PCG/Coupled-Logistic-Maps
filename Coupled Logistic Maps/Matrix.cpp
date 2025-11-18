c23dac8c32b46ee3af095f2c912e45db3f148c2b
Kristian Downes <kd741@bath.ac.uk>
2025-11-18 12:42:17 +0000

    In progress matrix calculations

--- Coupled Logistic Maps/Matrix.cpp
+++ Coupled Logistic Maps/Matrix.cpp
@@ -6,53 +6,66 @@
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/matrix_expression.hpp>
 #include <boost/numeric/ublas/io.hpp>
+#include "Graphing.h"
 
 void adjacency(int n) {
 
 }
 
+void stepGraph(float* mus, float* xs, float num) {
+
+	using namespace boost::numeric::ublas;
+	const static int startPop = 3;
+	static float initGrowthRates[startPop] = { 3.0f, 2.0f, 1.5f };
+	static float initCapacities[startPop] = { 2.0f, 1.0f, 3.0f };
+	static float initPopulation[startPop] = { 0.5f, 0.5f, 0.5f };
+	static vector<float> growthVector(startPop);
+	static vector<float> capacityVector(startPop);
+	static vector<float> populationVector(startPop);
+	static matrix<float> adjacencyMatrix(startPop, startPop);
+	static vector<float> logisticVector(startPop);
+	static int populations = startPop;
+	static float alpha = 0.1;
+
+
+	static bool hasRun = false;
+	if (!hasRun) {
+		hasRun = true;
+		for (int i = 0; i < 3; i++) {
+			growthVector[i] = initGrowthRates[i];
+			capacityVector[i] = initCapacities[i];
+			populationVector[i] = initPopulation[i];
+		}
+	}
+
+
+
+	for (int i = 0; i < populations; i++) {
+		for (int j = 0; j < populations; j++) {
+			//Create the adjacency matrix
+			if (i == j)
+				adjacencyMatrix(i, j) = -1.0f;
+			else if (i == 1)
+				adjacencyMatrix(i, j) = 1.0f;
+			else if (j == 1)
+				adjacencyMatrix(i, j) = 0.5f;
+			else
+				adjacencyMatrix(i, j) = 0.0f;
+		}
+		//Create the logistic vector
+		logisticVector[i] = growthVector[i] * populationVector[i] * (capacityVector[i] - populationVector[i]);
+	}
+	adjacencyMatrix = adjacencyMatrix * alpha;
+
+	static vector<float> result(populations, populations);
+	result = logisticVector + prod(adjacencyMatrix, populationVector);
+	populationVector = result;
+
+	std::cout << populationVector << std::endl;
+}
 
 void step() {
-	using namespace boost::numeric::ublas;
-	static matrix<float> adjacency(3, 3);
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (i == j)
-				adjacency(i, j) = -.1f;
-			else if (i == 1)
-				adjacency(i, j) = .1f;
-			else if (j == 1)
-				adjacency(i, j) = .05f;
-			else
-				adjacency(i, j) = .0f;
-		}
-	}
 
-	static identity_matrix<float> identityMatrix(3);
-	static matrix<float> positionMatrix(3,3);
-	static matrix<float> growthMatrix(3,3);
-	static matrix<float> capacityMatrix(3,3);
-	static vector<float> positionVector(3);
-
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			positionMatrix(i, j) = i == j ? 0.5f : 0;
-			growthMatrix(i, j) = i == j ? (i == 0 ? 1.0f : (i == 1 ? 2.0f : 1.8f)) : 0;
-			capacityMatrix(i, j) = i == j ? (i == 0 ? 2 : 1) : 0;
-		}
-		positionVector[i] = 0.5f;
-	}
-
-	static matrix<float> result(3, 3);
-	result = prod(prod(growthMatrix, capacityMatrix - positionMatrix) + adjacency, positionMatrix);
-
-	diagonal_matrix<double> m(3, &positionVector);
-
-	std::cout << result << std::endl;
-	/*
-	static vector<float> growthVector = (2.0f, 2.0f, 2.0f );
-	static vector<float> capacityVector = { 1.0f, 1.0f, 1.0f };
-	static vector<float> populationVector = { 0.5f, 0.5f, 0.5f };
-	growthVector + capacityVector*/
+	float a[3], b[3], c;
+	Graph(stepGraph, a, b, c);
 }
