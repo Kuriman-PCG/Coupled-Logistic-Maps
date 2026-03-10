1cbdc1dcd7af2c328e607159e425e5e9a325050b
James Gilbert <jg2593@bath.ac.uk>
2026-03-10 23:51:19 +0000

    Better envelope

--- Coupled Logistic Maps/Coupling.cpp
+++ Coupled Logistic Maps/Coupling.cpp
@@ -458,14 +458,14 @@ void coupledlogfunc(float* axs, float* ays, float a) {
 	float upper_envelope_x[101] = { 0 };
 	for (int i = 0; i < 101; i++) {
 		upper_envelope_r[i] = i / 25.0f;
-		upper_envelope_x[i] = (1.0f / (4.0f - 2.0f * alpha)) * upper_envelope_r[i];// +0.5f - 1.0f / (2.0f - 2.0f * alpha);
+		upper_envelope_x[i] = (1.0f / (4.0f - 4.0f * alpha)) * upper_envelope_r[i] + 0.5f - 1/(2.0f - 2.0f * alpha);
 	}
 
 	float lower_envelope_r[101] = { 0 };
 	float lower_envelope_x[101] = { 0 };
 	for (int i = 0; i < 101; i++) {
 		lower_envelope_r[i] = i / 25.0f;
-		lower_envelope_x[i] = og_step(upper_envelope_r[i], upper_envelope_x[i]) - alpha * (1 - (1 + 4 * alpha) * (og_step(upper_envelope_r[i], upper_envelope_x[i])));
+		lower_envelope_x[i] = (og_step(upper_envelope_r[i], upper_envelope_x[i]) - alpha * (upper_envelope_x[i] - og_step(upper_envelope_r[i], upper_envelope_x[i]) - alpha * upper_envelope_x[i])) / (1 + pow(alpha,2));
 	}
 
 	if(ImPlot::BeginSubplots("My Subplots", 2, 1, ImVec2(-1, -60), flagsa, rratios, cratios)) {
